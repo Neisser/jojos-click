a608a239fbb0e2527a2b2bb167a739aceb214e3e
Neisser <villa.neisser@gmail.com>
2024-12-29 10:01:35 -0500

    feat: add fastify and class validator

--- backend-services/src/main.ts
+++ backend-services/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  const port = process.env.PORT ?? 3000;
+
+  const host = '0.0.0.0';
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }))
+
+  await app.listen(port, host);
 }
 bootstrap();
