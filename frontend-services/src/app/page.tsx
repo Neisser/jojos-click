08c36fe3505b5fdce75ea6644697916cc9636b77
William Galvis <williamsoluntech@gmail.com>
2025-01-08 14:25:03 -0500

    fix responsive preview into some componente as button and layout
    change button navbar to github item button

--- frontend-services/src/app/page.tsx
+++ frontend-services/src/app/page.tsx
@@ -7,6 +7,7 @@ import Switch from "@/shared/components/switch";
 import { useFetch } from "@/hooks/useFetch";
 import { useRef, useState } from "react";
 import { Shortener } from "@/models/shortener.model";
+import Head from "next/head";
 
 export default function Home() {
     const [shortLink, setShortLink] = useState('');
@@ -25,32 +26,47 @@ export default function Home() {
             const selection = window.getSelection();
             selection?.removeAllRanges();
             selection?.addRange(range);
+
+            const text = spanRef.current.textContent || ""; // Obtiene el texto del elemento
+            navigator.clipboard.writeText(text) // Copia el texto al portapapeles
+                .then(() => {
+                    alert("Texto copiado al portapapeles!"); // Mensaje de confirmación
+                })
+                .catch((err) => {
+                    console.error("Error al copiar el texto: ", err);
+                });
         }
     }
 
     return (
-        <main>
-            <Navbar />
-            <section className={styles['page__content']}>
-                <h1></h1>
-                <p>Linkly is an efficient and easy-to-use URL shortening service that streamlines your online experience.</p>
-                <div className={styles['convert__container']}>
-                    <Input onChange={({ target: { value } }) => setShortLink(value)} onButtonClick={onShortLink} loading={isLoading} placeholder="Enter the link here" />
+        <>
+            <Head>
+                <title>jojo&apos;s</title>
+                <meta property="og:title" content="Shorten links" key="shorten" />
+            </Head>
+            <main>
+                <Navbar />
+                <section className={styles['page__content']}>
+                    <h1></h1>
+                    <p>Linkly is an efficient and easy-to-use URL shortening service that streamlines your online experience.</p>
+                    <div className={styles['convert__container']}>
+                        <Input onChange={({ target: { value } }) => setShortLink(value)} onButtonClick={onShortLink} loading={isLoading} placeholder="Enter the link here" />
 
-                    <section className={styles['options__container']}>
-                        <div className={styles['switch__container']}>
-                            <Switch />
-                            <p>Auto Paste from Clipboard</p>
-                        </div>
-                        {data?.short && <div>
-                            <label>Shorten link:</label>
-                        </div>}
-                        {data?.short &&
-                            <span ref={spanRef} style={{ cursor: 'pointer', userSelect: 'text' }} onClick={onSelectAll} className={styles['option__result']}>{data?.short}</span>
-                        }
-                    </section>
-                </div>
-            </section>
-        </main>
+                        <section className={styles['options__container']}>
+                            <div className={styles['switch__container']}>
+                                <Switch />
+                                <p>Auto Paste from Clipboard</p>
+                            </div>
+                            {data?.short && <div>
+                                <label>Shorten link:</label>
+                            </div>}
+                            {data?.short &&
+                                <span ref={spanRef} style={{ cursor: 'pointer', userSelect: 'text' }} onClick={onSelectAll} className={styles['option__result']}>{data?.short}</span>
+                            }
+                        </section>
+                    </div>
+                </section>
+            </main>
+        </>
     );
 }
